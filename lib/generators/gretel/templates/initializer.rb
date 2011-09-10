ed158238480cfd7d8c1262988de12833d804045d
Lasse Bunk <lassebunk@gmail.com>
2011-09-10 17:19:19 +0200

    move from install.rb to generator

---
+++ lib/generators/gretel/templates/initializer.rb
@@ -0,0 +1,30 @@
+Gretel::Crumbs.layout do
+  
+  # Remember to restart your application after editing this file.
+  
+  # Example crumbs:
+  
+  # crumb :root do
+  #   link "Home", root_path
+  # end
+  
+  # crumb :projects do
+  #   link "Projects", projects_path
+  # end
+  
+  # crumb :project do |project|
+  #   link lambda { |project| "#{project.name} (#{project.id.to_s})" }, project_path(project)
+  #   parent :projects
+  # end
+  
+  # crumb :project_issues do |project|
+  #   link "Issues", project_issues_path(project)
+  #   parent :project, project
+  # end
+  
+  # crumb :issue do |issue|
+  #   link issue.name, issue_path(issue)
+  #   parent :project_issues, issue.project
+  # end
+
+end
