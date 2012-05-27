ce05330f598a9e509ba643c7f6b9cf3f7d5669e3
Lasse Bunk <lassebunk@gmail.com>
2012-05-27 15:25:02 +0200

    support blank urls

--- lib/gretel/helper_methods.rb
+++ lib/gretel/helper_methods.rb
@@ -83,30 +83,34 @@ module Gretel
       
       out = []
       while link = links.shift
-        if options[:use_microformats]
-          out << content_tag(:div, link_to(content_tag(:span, link.text, :itemprop => "title"), link.url, link.options.merge(:itemprop => "url")), :itemscope => "", :itemtype => "http://data-vocabulary.org/Breadcrumb")
-        else
-          out << link_to(link.text, link.url)
-        end
+        out << get_crumb(link.text, link.url, options[:use_microformats], "", link.options)
       end
       
       if last_link
         if options[:link_last] || options[:link_current]
-          if options[:use_microformats]
-            out << content_tag(:div, link_to(content_tag(:span, last_link.text, :class => "current", :itemprop => "title"), last_link.url, last_link.options.merge(:itemprop => "url")), :itemscope => "", :itemtype => "http://data-vocabulary.org/Breadcrumb")
-          else
-            out << link_to(last_link.text, last_link.url, :class => "current")
-          end
+          out << get_crumb(last_link.text, last_link.url, options[:use_microformats], "current", last_link.options)
         else
-          if options[:use_microformats]
-            out << content_tag(:div, content_tag(:span, last_link.text, :class => "current", :itemprop => "title"), :itemscope => "", :itemtype => "http://data-vocabulary.org/Breadcrumb")
-          else
-            out << content_tag(:span, last_link.text, :class => "current")
-          end
+          out << get_crumb(last_link.text, nil, options[:use_microformats], "current")
         end
       end
       
       out.join(options[:separator] || " &gt; ").html_safe
+    end
+    
+    def get_crumb(text, url, use_microformats, css_class, options = {})
+      if url.blank?
+        if use_microformats
+          content_tag(:div, content_tag(:span, text, :class => css_class, :itemprop => "title"), :itemscope => "", :itemtype => "http://data-vocabulary.org/Breadcrumb")
+        else
+          content_tag(:span, text, :class => css_class)
+        end
+      else
+        if use_microformats
+          content_tag(:div, link_to(content_tag(:span, text, :class => css_class, :itemprop => "title"), url, options.merge(:itemprop => "url")), :itemscope => "", :itemtype => "http://data-vocabulary.org/Breadcrumb")
+        else
+          link_to(text, url, :class => css_class)
+        end
+      end
     end
   end
 end
