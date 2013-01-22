fcc081aa4196e45bd7ca5e70295c1a8ad0b0757c
Lasse Bunk <lassebunk@gmail.com>
2013-01-22 17:29:09 +0100

    Set up routes

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,11 +1,23 @@
 Dummy::Application.routes.draw do
-  root :to => "home#index"
+  root :to => "home#root"
 
-  match "about" => "home#about", :as => :about
-  match "about/contact", "home#contact", :as => :contact
-  match "about/contact/form", "home#contact_form", :as => :contact_form
+  match "about"              => "home#about",         :as => :about
+  match "about/contact"      => "home#contact",       :as => :contact
+  match "about/contact/form" => "home#contact_form",  :as => :contact_form
 
   resources :projects do
     resources :issues
+  end
+
+  [ :root,
+    :simple,
+    :with_root,
+    :without_root,
+    :with_autopath,
+    :with_parent,
+    :with_parent_object,
+    :multiple_links,
+    :multiple_links_with_parent ].each do |action|
+      match action.to_s => "home##{action}", :as => action
   end
 end
