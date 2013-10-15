8d937feb100e1e5c70e9cfa0760fe2d5be078885
Lasse Bunk <lassebunk@gmail.com>
2013-10-15 20:26:26 +0200

    Use class << self

--- lib/gretel/trail/store.rb
+++ lib/gretel/trail/store.rb
@@ -1,19 +1,21 @@
 module Gretel
   module Trail
     class Store
-      # Encode array of +links+ to unique trail key.
-      def self.encode(links)
-        raise "#{self.name} must implement #encode to be able to encode trails."
-      end
+      class << self
+        # Encode array of +links+ to unique trail key.
+        def encode(links)
+          raise "#{name} must implement #encode to be able to encode trails."
+        end
 
-      # Decode unique trail key to array of links.
-      def self.decode(key)
-        raise "#{self.name} must implement #decode to be able to decode trails."
-      end
-  
-      # Resets all changes made to the store. Used for testing.
-      def self.reset!
-        instance_variables.each { |var| remove_instance_variable var }
+        # Decode unique trail key to array of links.
+        def decode(key)
+          raise "#{name} must implement #decode to be able to decode trails."
+        end
+    
+        # Resets all changes made to the store. Used for testing.
+        def reset!
+          instance_variables.each { |var| remove_instance_variable var }
+        end
       end
     end
   end
