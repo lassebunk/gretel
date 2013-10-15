a7b1691069e9c9152b052ba3a8ad4cf47e1d3b55
Lasse Bunk <lassebunk@gmail.com>
2013-10-15 22:55:27 +0200

    Simpler way of defining rake tasks

--- lib/gretel/trail/tasks.rb
+++ lib/gretel/trail/tasks.rb
@@ -1,21 +1,5 @@
 require "rake"
 
-module Gretel
-  module Trail
-    module Tasks
-      extend Rake::DSL
-
-      def self.install
-        namespace :gretel do
-          namespace :trails do
-            task :delete_expired => :environment do
-              Gretel::Trail.delete_expired
-            end
-          end
-        end
-      end
-    end
-  end
+Rake::Task.define_task("gretel:trails:delete_expired" => :environment) do
+  Gretel::Trail.delete_expired
 end
-
-Gretel::Trail::Tasks.install
