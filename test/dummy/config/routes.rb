f1042aa7ca792e658f41dba4b028b4c0a1d219a4
Masato Ikeda <masato.ikeda@gmail.com>
2014-05-10 03:17:22 +0900

    Replace `match` routing methods with `get` in dummy app

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,9 +1,9 @@
 Dummy::Application.routes.draw do
   root to: "dummy#dummy"
 
-  match "about"              => "dummy#dummy", as: :about
-  match "about/contact"      => "dummy#dummy", as: :contact
-  match "about/contact/form" => "dummy#dummy", as: :contact_form
+  get "about"              => "dummy#dummy", as: :about
+  get "about/contact"      => "dummy#dummy", as: :contact
+  get "about/contact/form" => "dummy#dummy", as: :contact_form
 
   resources :projects do
     resources :issues
