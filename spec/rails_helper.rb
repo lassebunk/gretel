f6e239953eea551d01075d36feb94df291817137
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2020-08-23 14:52:49 +0900

    move dummy and fixtures from test to spec directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../test/dummy/config/environment', __dir__)
+require File.expand_path('dummy/config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -32,8 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.fixture_path = File.expand_path('../test/fixtures', __dir__)
+  config.fixture_path = File.expand_path('fixtures', __dir__)
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
